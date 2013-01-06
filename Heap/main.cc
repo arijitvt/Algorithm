0dc048d6b66c6ad6b470c4b8041c53bd27a29ef1
arijit <arijitnit06@gmail.com>
2013-01-06 17:06:11 -0500

    Heap Sort completed

--- Heap/main.cc
+++ Heap/main.cc
@@ -2,7 +2,8 @@
 using namespace std;
 
 int a[] =  {16,4,10,14,7,9,3,2,8,1};
-const int SIZE = 10;
+ int SIZE = 10;
+const int FIX_SIZE = SIZE;
 
 int leftChild(int index){
   return (2*index)+1;
@@ -36,23 +37,29 @@ void Max_Heapify(int index){
   }
 }
 
-
-void showArray(){
-
-  for(int i= 0; i < SIZE; ++i){
-    cout<<a[i]<<"\t";
-  }
-  cout<<endl;
-}
-
 void Build_Heap(){
   for(int i = SIZE/2 ;i >= 1 ; --i ){
    Max_Heapify(i);
   }
 }
 
+void showArray(){
+  for(int i= 0; i < FIX_SIZE; ++i){
+    cout<<a[i]<<"\t";
+  }
+  cout<<endl;
+}
+
+// This function will do the heap sort.
 int main(){
   Build_Heap();
-  showArray();
+  for(int i= SIZE-1; i >= 1; i-- ){
+    int temp = a[0];
+    a[0] = a[i];
+    a[i] = temp;
+    SIZE -- ;
+    Max_Heapify(0);
+  }
+ showArray();
   return 0;
 }
